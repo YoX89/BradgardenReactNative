e5cf059dcbcd20c4557ef5e530aea4e35ec715a9
Jonathan Hagberg <jonathan.hagberg89@gmail.com>
2018-01-30 18:41:36 +0100

    Adds a first version of a general selection screen

--- Screens/AddSessionScreen.js
+++ Screens/AddSessionScreen.js
@@ -2,22 +2,23 @@ import React, { Component } from "react";
 import {
   AppRegistry,
   View,
-  FlatList,
   Text,
   ActivityIndicator,
-  StyleSheet,
-  Image
+  ScrollView
 } from "react-native";
 import { ContainerStyles } from "./Styles/ContainerStyles";
 import { RowStyles } from "./Styles/RowStyles";
 import { ButtonStyles } from "./Styles/ButtonStyles";
+import Button from "../Components/Button";
 import Api from "../Networking/Api";
+import SelectionScreen from "./SelectionScreen";
 
 export default class AddSessionScreen extends Component {
   constructor() {
     super();
     this.state = {
-      loading: false
+      loading: false,
+      selectedGame: null
     };
   }
 
@@ -32,7 +33,7 @@ export default class AddSessionScreen extends Component {
   };
 
   render() {
-    const { loading } = this.state;
+    const { loading, selectedGame } = this.state;
 
     if (loading) {
       return (
@@ -42,8 +43,54 @@ export default class AddSessionScreen extends Component {
       );
     }
 
-    return <Text>Here you should be able to add a session</Text>;
+    return (
+      <ScrollView>
+        <Button title="Choose game" onPress={() => this.chooseGame()} />
+        <Button title="Choose winners" onPress={() => this.chooseWinners()} />
+        <Button title="Choose losers" onPress={() => this.chooseLosers()} />
+        {selectedGame &&
+          selectedGame.hasTraitor && (
+            <Button
+              title="Choose traitors"
+              onPress={() => this.chooseTraitors()}
+            />
+          )}
+        <SelectionScreen
+          isVisible={false}
+          selectables={[]}
+          onRef={ref => (this.selectionScreen = ref)}
+        />
+      </ScrollView>
+    );
   }
+
+  chooseGame = async () => {
+    var games = await Api.fetchGames();
+    games = games.map(game => {
+      game.text = game.name;
+      return game;
+    });
+    this.selectionScreen.setSelectables(games);
+    this.selectionScreen.toggleVisible();
+  };
+
+  chooseWinners() {
+    this.openMembers();
+  }
+
+  chooseLosers() {
+    this.openMembers();
+  }
+
+  openMembers = async () => {
+    var members = await Api.fetchMembers();
+    members = members.map(member => {
+      member.text = member.firstName + " " + member.lastName;
+      return member;
+    });
+    this.selectionScreen.setSelectables(members);
+    this.selectionScreen.toggleVisible();
+  };
 }
 
 AppRegistry.registerComponent("AddSessionScreen", () => AddSessionScreen);
---
+++ Screens/SelectionScreen.js
@@ -0,0 +1,54 @@
+import React, { PureComponent } from "react";
+import { AppRegistry, View, FlatList, Text, SafeAreaView } from "react-native";
+import Modal from "react-native-modal";
+import Button from "../Components/Button";
+import { ContainerStyles } from "./Styles/ContainerStyles";
+import { RowStyles } from "./Styles/RowStyles";
+
+const extractKey = ({ id }) => id;
+
+export default class SelectionScreen extends PureComponent {
+  constructor(props) {
+    super();
+    this.state = {
+      selectables: props.selectables,
+      isVisible: props.isVisible
+    };
+  }
+
+  componentDidMount() {
+    this.props.onRef(this);
+  }
+
+  toggleVisible() {
+    this.setState({ isVisible: !this.state.isVisible });
+  }
+
+  setSelectables(selectables) {
+    this.setState({ selectables: selectables });
+  }
+
+  renderSelectable = ({ item }) => {
+    return <Text style={RowStyles.row}>{item.text}</Text>;
+  };
+
+  render() {
+    const { isVisible, selectables } = this.state;
+
+    return (
+      <Modal isVisible={isVisible}>
+        <SafeAreaView style={ContainerStyles.modal}>
+          <Button title="Done" onPress={() => this.toggleVisible()} />
+          <FlatList
+            style={ContainerStyles.full}
+            data={selectables}
+            renderItem={this.renderSelectable}
+            keyExtractor={extractKey}
+          />
+        </SafeAreaView>
+      </Modal>
+    );
+  }
+}
+
+AppRegistry.registerComponent("SelectionScreen", () => SelectionScreen);
