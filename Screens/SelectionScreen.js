54ea7347fcf7fc4efe0b434077dc0091ba140323
Jonathan Hagberg <jonathan.hagberg89@gmail.com>
2018-02-07 14:59:26 +0100

    De-statify selection screen

--- Screens/SelectionScreen.js
+++ Screens/SelectionScreen.js
@@ -18,36 +18,20 @@ export default class SelectionScreen extends PureComponent {
   constructor(props) {
     super();
     this.state = {
-      isVisible: props.isVisible,
-      selectables: props.selectables,
-      onPressDone: props.onPressDone,
-      selectedIds: props.selectedIds,
-      selectMultiple: props.selectMultiple
+      selectedIds: props.selectedIds
     };
   }
 
   componentWillReceiveProps(nextProps) {
-    if (this.state.isVisible != nextProps.isVisible) {
-      this.setState({ isVisible: nextProps.isVisible });
-    }
-    if (this.state.selectables != nextProps.selectables) {
-      this.setState({ selectables: nextProps.selectables });
-    }
     if (this.state.selectedIds != nextProps.selectedIds) {
       this.setState({ selectedIds: nextProps.selectedIds });
-    }
-    if (this.state.selectMultiple != nextProps.selectMultiple) {
-      this.setState({ selectMultiple: nextProps.selectMultiple });
-    }
-    if (this.state.onPressDone != nextProps.onPressDone) {
-      this.setState({ onPressDone: nextProps.onPressDone });
     }
   }
 
   onPressItem = id => {
     const { selectedIds } = this.state;
 
-    if (this.state.selectMultiple) {
+    if (this.props.selectMultiple) {
       const index = selectedIds.indexOf(id);
       const newSelected = selectedIds.splice(0);
       if (index != -1) {
@@ -73,12 +57,8 @@ export default class SelectionScreen extends PureComponent {
   };
 
   onPressDone = () => {
-    const {
-      selectables,
-      selectedIds,
-      selectMultiple,
-      onPressDone
-    } = this.state;
+    const { selectedIds } = this.state;
+    const { selectables, onPressDone } = this.props;
     const selectedObjects = selectables.filter(
       selectable => selectedIds.indexOf(selectable.id) != -1
     );
@@ -86,7 +66,7 @@ export default class SelectionScreen extends PureComponent {
   };
 
   render() {
-    const { isVisible, selectables } = this.state;
+    const { isVisible, selectables } = this.props;
 
     return (
       <Modal isVisible={isVisible}>
