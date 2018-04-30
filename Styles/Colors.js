2c36b49efe598526ae2847b27b16e68f24c3f6e3
Jonathan Hagberg <jonathan.hagberg89@gmail.com>
2018-04-30 16:33:55 +0200

    Updates colors

--- Styles/Colors.js
+++ Styles/Colors.js
@@ -1,11 +1,11 @@
 export const Colors = {
   background: "#FFFFFF",
-  bar: "#0A6D73",
+  bar: "#E20613",
   primary: "#FFFFFF",
-  secondary: "#00A099",
-  secondaryOverlay: "#00A09966",
+  secondary: "#7FB7BE",
+  secondaryOverlay: "#7FB7BE66",
   button: "#F29100",
   black: "#000000",
   error: "#FF0000",
-  divider: "#9B9B9B44"
+  divider: "#7FB7BE44"
 };
